b3771b0e2bcc55a1302e117a930930b33e73ff5a
ManikumarJ <manikumarj6@gmail.com>
2026-06-12 15:20:14 +0530

    Added

---
+++ Day-5/importExport/app.js
@@ -0,0 +1,12 @@
+// Import / Export (Real Interview Task) 
+// File 1 math.js Create: add() sub() Export them.
+//  File 2 app.js Import both functions and call them. 
+//  Expected Output 30 10
+
+import {add, sub} from "./math.js"
+
+console.log(add(20, 10));
+console.log(sub(20, 10));
+
+
+
