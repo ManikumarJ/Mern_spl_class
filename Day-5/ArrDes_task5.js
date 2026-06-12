fff4766381997e0b2510083585d4a1bb1358159e
ManikumarJ <manikumarj6@gmail.com>
2026-06-12 12:49:36 +0530

    Added

---
+++ Day-5/ArrDes_task5.js
@@ -0,0 +1,12 @@
+// Task 5:
+//  Array Destructuring 
+//   const colors = ["Red", "Blue", "Green"]; 
+// Task Using Array Destructuring: Store first two colors in separate variables.
+
+const colors = ["Red", "Blue", "Green"]; 
+
+const [color1 , color2] = colors
+
+console.log(color1);
+console.log(color2);
+
