2e1f9440c09fd64e611a1a20fc88b2018821aa08
ManikumarJ <manikumarj6@gmail.com>
2026-06-12 15:09:31 +0530

    Added

---
+++ Day-5/EnhanceObj_task7.js
@@ -0,0 +1,9 @@
+// Enhanced Object Literal  
+// const name = "Priya"; 
+// const role = "React Developer"; 
+// Task Create object using ES6 shorthand property.
+
+const name = "Priya"; 
+const role = "React Developer"; 
+
+
---
+++ Day-5/NullCol_task8.js
@@ -0,0 +1,9 @@
+// Nullish Coalescing Operator 
+//  const userName = null; 
+//  Task If value is null or undefined 
+//  show: Guest User Using ?? Expected Output Guest User
+
+const userName = null; 
+const res = userName ?? "Guest"
+
+console.log(`Welcome ${res}`);
