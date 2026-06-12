6623c2814907fe8cb4b397b60a0fdbd2a1070c08
ManikumarJ <manikumarj6@gmail.com>
2026-06-12 12:42:49 +0530

    Added

---
+++ Day-5/TempLit_task3.js
@@ -0,0 +1,13 @@
+// Task 3: Template Literals 
+// const name = "Ravi"; 
+// const role = "MERN Developer"; 
+// const company = "Infosys"; 
+// Task Using Template Literals 
+
+const name = "Ravi"; 
+const role = "MERN Developer"; 
+const company = "Infosys"; 
+
+console.log(`Name : ${name}
+Role : ${role}
+Company Name : ${company}`);
